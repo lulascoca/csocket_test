beeac34e38699fc009b438f473b30babf31f5746
lulascoca <lulascocainfinity@gmail.com>
2019-04-09 23:42:37 +0100

    finally was able to understand binding in c XD

--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define PORT 1997
@@ -9,19 +10,18 @@ int main(){
   struct sockaddr_in address;
   //passing "args" to address
   address.sin_family = AF_INET;
-  address.sin_port = htons(PORT);
-  inet_aton("192.168.1.83", &myaddr.sin_addr.s_addr);
+  address.sin_port = htons( PORT );
+  address.sin_addr.s_addr = htonl(INADDR_ANY);
 
   //creating buffer
   char buffer[1024];
-  memset(buffer, 0, sizeof(buffer));
+  memset(&buffer, 0, sizeof(buffer));
 
   int addrlen = sizeof(address);
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  int setsockopt(sockfd);
 
-  int bind(sockfd, *address, );
-
+  // bind socket
+  bind(sockfd, (struct sockaddr *)&address, sizeof(address));
   return 0;
 }
