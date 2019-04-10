f75e40cbdadab5f2816a67cab9428ddc66a099be
lulascoca <lulascocainfinity@gmail.com>
2019-04-10 11:58:09 +0100

    finished server and client bases :D

---
+++ client/client.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <syscall.h>
+#include <unistd.h>
+
+#define PORT 8000
+
+int main(){
+  char *message = "hello from the other siiide";
+
+  char buffer[1024];
+  memset(&buffer, 0, sizeof(buffer));
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  //create struct for server address
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons( PORT );
+  inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+  //connect
+  connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+  //send data i guess XD
+  send(sock, message, strlen(message), 0);
+  printf("message sent\n");
+
+  read(sock, &buffer, sizeof(buffer));
+  printf("Server says: %s\n", buffer);
+
+  return 0;
+}
--- server/server.cpp
+++ server/server.cpp
@@ -1,27 +1,47 @@
 #include <stdio.h>
-#include <string.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <syscall.h>
+#include <unistd.h>
 
-#define PORT 1997
+#define PORT 8000
 
 int main(){
+  char *message = "hello";
+
   // defining struct address from existing struct in socket.h sockaddr_on
+  const int backlog = 3;
   struct sockaddr_in address;
   //passing "args" to address
   address.sin_family = AF_INET;
   address.sin_port = htons( PORT );
   address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  //creating buffer
-  char buffer[1024];
-  memset(&buffer, 0, sizeof(buffer));
+  socklen_t addrlen = sizeof(address);
 
-  int addrlen = sizeof(address);
+  //creating buffer
+  char buffer[8192];
+  memset(&buffer, 0, sizeof(buffer));
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   // bind socket
   bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+
+  printf("listening on port %d\n", PORT);
+  if (listen(sockfd, backlog) < 0){
+    printf("bind failed");
+  }
+
+  int new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+  //valread = read(sockfd, &buffer, sizeof(buffer));
+  int valread = read(new_socket, buffer, sizeof(buffer));
+  printf("Client says: %s\n", buffer);
+  send(new_socket, message, strlen(message), 0);
+  printf("\nmessage sent\n");
+
   return 0;
 }
