5b1f29de725e933eed2bac6ceedae73ef70fd362
lulascoca <lulascocainfinity@gmail.com>
2019-04-10 18:35:32 +0100

    added command line argumments to the server part ;)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # csocket_test
+
+just my very simple attempt at socket programming in c and maybe in the
+future some sort of asymetrical encryption with some python help and bash ;)
--- server/server.cpp
+++ server/server.cpp
@@ -5,26 +5,51 @@
 #include <string.h>
 #include <syscall.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
+#define PASS (void)0
 #define PORT 8000
 
-int main(){
-  char *message = "hello";
+void keep_alive(){
 
-  // defining struct address from existing struct in socket.h sockaddr_on
+  PASS;
+
+}
+
+void help(){
+
+printf("hey\nusage:\nserver 'address'\nbai\n");
+
+}
+
+int main(int argc, char** argv){
+  char *message = "hello";
+  if (argc > 1){
+    PASS;
+  }
+  else{
+    help();
+    exit(0);
+  }
+
+  // defining struct address from existing struct in socket.h sockaddr_in,
+  // backlog is the ammount of "unprocessed requests"
   const int backlog = 3;
   struct sockaddr_in address;
+
   //passing "args" to address
   address.sin_family = AF_INET;
   address.sin_port = htons( PORT );
-  address.sin_addr.s_addr = htonl(INADDR_ANY);
+  inet_pton(AF_INET, argv[1], &address.sin_addr);
 
-  socklen_t addrlen = sizeof(address);
+  //passing length of address to a variable that is of the data type socklen_t
+  int addrlen = sizeof(address);
 
   //creating buffer
   char buffer[8192];
   memset(&buffer, 0, sizeof(buffer));
 
+  //creating a socket from the class/object socket from <sys/socket.h>
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   // bind socket
